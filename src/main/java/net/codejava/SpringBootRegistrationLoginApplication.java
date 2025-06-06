86159c6d329a7cd7199fe25b0a2face558db79b9
renuka1217 <90893936+renuka1217@users.noreply.github.com>
2025-06-06 19:42:15 +0530

    Update SpringBootRegistrationLoginApplication.java

--- src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
+++ src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
@@ -2,6 +2,8 @@ package net.codejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "net.codejava")
