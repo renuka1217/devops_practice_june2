79893a1801f49cbf19746e7eaf9839f9dda67431
renuka1217 <90893936+renuka1217@users.noreply.github.com>
2025-06-07 20:39:23 +0530

    Create CorsGlobalConfig.java

---
+++ src/main/java/com/login/user/CorsGlobalConfig.java
@@ -0,0 +1,21 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "http://192.168.23.131:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
