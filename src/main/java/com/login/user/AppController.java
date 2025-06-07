ebf2d819a81e40d132475ae03ee94bbe6c91f6fd
renuka1217 <90893936+renuka1217@users.noreply.github.com>
2025-06-07 20:39:46 +0530

    Update AppController.java

--- src/main/java/com/login/user/AppController.java
+++ src/main/java/com/login/user/AppController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://192.168.23.131:5173")
+//@CrossOrigin(origins = "http://192.168.23.131:5173")
 @RestController
 @RequestMapping("/")
 public class AppController {
