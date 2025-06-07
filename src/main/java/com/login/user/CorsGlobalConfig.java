e5b2580e6e09166a2c5fe02902a04fe77d2613f9
renuka1217 <90893936+renuka1217@users.noreply.github.com>
2025-06-07 21:01:56 +0530

    Update CorsGlobalConfig.java

--- src/main/java/com/login/user/CorsGlobalConfig.java
+++ src/main/java/com/login/user/CorsGlobalConfig.java
@@ -13,7 +13,7 @@ public class CorsGlobalConfig {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://192.168.23.131:5173")
+                        .allowedOrigins("http://localhost:5174", "http://192.168.23.131:5174")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
