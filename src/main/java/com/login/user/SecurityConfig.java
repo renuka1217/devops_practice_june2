f64270d0cda7a7fc7b5d3fa06c342afb7cab6d42
renuka1217 <90893936+renuka1217@users.noreply.github.com>
2025-06-25 18:35:18 +0530

    Update SecurityConfig.java

--- src/main/java/com/login/user/SecurityConfig.java
+++ src/main/java/com/login/user/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Arrays;
 import java.util.List;
